b326a5a0588b06e90e9954b62f16d55918d28186
hong2828 <hongb2828@gmail.com>
2024-04-14 22:05:35 +0800

    feat: created project

---
+++ src/test/java/hong2828/banking/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hong2828.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
